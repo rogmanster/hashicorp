115626aeb3331e5147422d95fba3ddc35542f166
Roger Chao <rogman@gmail.com>
2019-01-08 13:29:42 -0800

    Update install-consul.sh

--- customizations/terraform-guides/vagrant-hashistack-consul-dev/install-consul.sh
+++ customizations/terraform-guides/vagrant-hashistack-consul-dev/install-consul.sh
@@ -33,7 +33,10 @@ sudo tee /etc/consul.d/acl.json > /dev/null  <<ACL
   "acl" : {
     "enabled": true,
     "default_policy": "deny",
-    "down_policy": "extend-cache"
+    "down_policy": "extend-cache",
+    "tokens" : {
+      "master" : "b1gs33cr3t"
+    }
   }
 }
 ACL
