afccc81b7c3046ef085a40a0a21f643b50459a0e
Roger Chao <rogman@gmail.com>
2019-01-14 20:43:54 -0800

    Update main.tf

--- customizations/vault-guides/vault-aws/main.tf
+++ customizations/vault-guides/vault-aws/main.tf
@@ -90,7 +90,7 @@ resource "aws_launch_configuration" "vault" {
 module "vault_lb_aws" {
   # source = "github.com/hashicorp-modules/vault-lb-aws"
   # rogmanster - customized to disble aws lb s3 bucket
-  source = "https://github.com/rogmanster/hashicorp/tree/master/customizations/vault-guides/vault-lb-aws"
+  source = "github.com/rogmanster/hashicorp/tree/master/customizations/vault-guides/vault-lb-aws"
 
   create             = "${var.create}"
   name               = "${var.name}"
