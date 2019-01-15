177449a90a8a7ed5986ecc857be3b776fa16f350
Roger Chao <rogman@gmail.com>
2019-01-15 15:08:03 -0800

    Delete main.tf

--- customizations/vault-guides/vault-lb-aws/examples/no-provision/main.tf
+++
@@ -1,9 +0,0 @@
-module "vault_lb_aws" {
-  # source = "github.com/hashicorp-modules/vault-lb-aws"
-  source = "../../../vault-lb-aws"
-
-  create      = false
-  vpc_id      = ""
-  cidr_blocks = []
-  subnet_ids  = []
-}
